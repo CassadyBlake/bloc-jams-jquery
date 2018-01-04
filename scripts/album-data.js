b3472eacd7348dd688ce710d8ada41f6141c1cdd
CassadyBlake <cassady.lillstrom@gmail.com>
2018-01-03 16:45:10 -0800

    checkpoint 13 assignment: completed

--- scripts/album-data.js
+++ scripts/album-data.js
@@ -1,13 +1,13 @@
 const album = {
-  title: 'The Colors',
-  artist: 'Pablo Picasso',
-  releaseInfo: '1909 Spanish Records',
-  albumArtUrl: 'assets/images/album_covers/01.png',
+  title: 'Lovejoys',
+  artist: 'Pickwick',
+  releaseInfo: 'Small Press Records',
+  albumArtUrl: 'assets/images/album_covers/22.jpg',
   songs: [
-    { title: 'Blue', duration: '161.71', soundFileUrl: 'assets/music/blue.mp3' },
-    { title: 'Green', duration: '103.96', soundFileUrl: 'assets/music/green.mp3' },
-    { title: 'Red', duration: '268.45', soundFileUrl: 'assets/music/red.mp3' },
-    { title: 'Pink', duration: '153.14', soundFileUrl: 'assets/music/pink.mp3' },
-    { title: 'Magenta', duration: '374.22', soundFileUrl: 'assets/music/magenta.mp3' }
+    { title: 'Thought It Was You', duration: '161.71', soundFileUrl: 'assets/music/Thought.mp3' },
+    { title: 'Turncoat', duration: '103.96', soundFileUrl: 'assets/music/Turncoat.mp3' },
+    { title: 'In Time', duration: '268.45', soundFileUrl: 'assets/music/InTime.mp3' },
+    { title: 'Ascension', duration: '153.14', soundFileUrl: 'assets/music/Ascension.mp3' },
+    { title: 'Lying Awake In The Dark', duration: '374.22', soundFileUrl: 'assets/music/Lying.mp3' }
   ]
 };
